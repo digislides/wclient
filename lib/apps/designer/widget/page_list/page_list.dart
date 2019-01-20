17c9c518c434baf25ea0ec8b5ce54fe8b0c5279d
tejainece <tejainece@gmail.com>
2019-01-20 22:42:29 +0100

    Page list

--- lib/apps/designer/widget/page_list/page_list.dart
+++ lib/apps/designer/widget/page_list/page_list.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 
+import 'package:common/models.dart';
+
 @Component(
   selector: 'page-list',
   styleUrls: ['page_list.css'],
@@ -13,5 +15,56 @@ import 'package:angular_components/angular_components.dart';
   ],
 )
 class PageListComponent {
+  @Input()
+  List<Page> pages = [
+    Page(
+        id: '1',
+        name: 'Page1',
+        width: 200,
+        height: 300,
+        duration: 5,
+        fit: Fit.cover,
+        items: [
+          TextItem(
+              id: '0',
+              text: "Hello!",
+              left: 10,
+              top: 20,
+              width: 100,
+              height: 50,
+              color: 'red',
+              font: FontProperties(size: 25)),
+          ImageItem(
+              id: '1',
+              name: 'Image',
+              left: 10,
+              top: 50,
+              width: 150,
+              height: 100,
+              url:
+                  'http://as01.epimg.net/en/imagenes/2018/03/04/football/1520180124_449729_noticia_normal.jpg',
+              fit: Fit.cover),
+        ]),
+    Page(
+        id: '2',
+        name: 'Page2',
+        width: 200,
+        height: 300,
+        duration: 5,
+        fit: Fit.cover,
+        items: [
+          ImageItem(
+              id: '1',
+              name: 'Image',
+              left: 10,
+              top: 50,
+              width: 150,
+              height: 100,
+              url:
+                  'http://as01.epimg.net/en/imagenes/2018/03/04/football/1520180124_449729_noticia_normal.jpg',
+              fit: Fit.cover),
+        ])
+  ];
+
   PageListComponent();
 }
