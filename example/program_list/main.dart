592402c6c99e983912e17b97156b57663605333a
tejainece <tejainece@gmail.com>
2019-01-23 15:27:39 +0100

    ProgramList

---
+++ example/program_list/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+import 'package:wclient/apps/dashboard/widgets/program_list/program_list.template.dart'
+    as ng;
+
+void main() {
+  runApp(ng.ProgramListComponentNgFactory);
+}
--- lib/apps/dashboard/widgets/program_list/program_list.dart
+++ lib/apps/dashboard/widgets/program_list/program_list.dart
@@ -1,6 +1,32 @@
 import 'package:angular/angular.dart';
 import 'package:common/models.dart';
 
+import 'package:wclient/utils/directives/input_binder.dart';
+
+class Paginated<M> {
+  int page;
+
+  int numPerPage;
+
+  int totalPages;
+
+  List<M> items;
+
+  Paginated({
+    this.page,
+    this.numPerPage,
+    this.totalPages,
+    this.items,
+  });
+}
+
+class ProgramListService {
+  Future<Paginated<Program>> getPrograms(
+      String search, int numPerPage, int page) async {
+    // TODO
+  }
+}
+
 @Component(
   selector: 'program-list',
   styleUrls: ['program_list.css'],
@@ -8,14 +34,31 @@ import 'package:common/models.dart';
   directives: [
     NgFor,
     NgIf,
+    TextBinder,
   ],
 )
 class ProgramListComponent {
   /// List of programs
-  List<Program> programs;
+  Paginated programs = Paginated(
+    page: 0,
+    numPerPage: 6,
+    items: [
+      Program(name: 'Program1'),
+      Program(name: 'Program2'),
+      Program(name: 'Program3'),
+      Program(name: 'Program4'),
+      Program(name: 'Program5'),
+      Program(name: 'Program6'),
+    ],
+    totalPages: 6,
+  );
 
   /// List of recent programs
-  List<Program> recent;
+  List<Program> recent = [
+    Program(name: 'Program1'),
+    Program(name: 'Program2'),
+    Program(name: 'Program3'),
+  ];
 
   ProgramListComponent();
 }
