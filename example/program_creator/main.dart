0de91ce09bace70cf5d97b10b12b565bf7a95052
tejainece <tejainece@gmail.com>
2019-01-23 11:20:58 +0100

    ProgramCreator widget

---
+++ example/program_creator/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+import 'package:wclient/apps/dashboard/widgets/program_creator/program_creator.template.dart'
+    as ng;
+
+void main() {
+  runApp(ng.ProgramCreatorComponentNgFactory);
+}
---
+++ lib/apps/dashboard/widgets/program_creator/program_creator.dart
@@ -0,0 +1,30 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'package:wclient/utils/directives/input_binder.dart';
+
+import 'package:common/models.dart';
+
+@Component(
+    selector: 'program-creator',
+    styleUrls: ['program_creator.css'],
+    templateUrl: 'program_creator.html',
+    directives: [
+      NgFor,
+      NgIf,
+      TextBinder,
+      SelectBoxBinder,
+    ],)
+class ProgramCreatorComponent {
+  final model = ProgramCreator();
+
+  ProgramCreatorComponent();
+
+  Future<void> create() async {
+    // TODO
+  }
+
+  void reset() {
+    model.reset();
+  }
+}
