283d51a3b6d2194b99b6e85449d31e64e2e49637
tejainece <tejainece@gmail.com>
2019-02-27 16:46:23 +0100

    Play and preview

---
+++ example/play/program/play/main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:jaguar_resty/jaguar_resty.dart';
+import 'package:http/browser_client.dart';
+
+import 'package:common/models.dart';
+import 'package:player/preview.dart';
+import 'package:player/utils/api/api.dart';
+
+main() async {
+  globalClient = BrowserClient();
+
+  final uri = Uri.parse(window.location.href);
+  String id = uri.queryParameters['id'];
+
+  Program program = await programApi.getById(id);
+  if (program == null) {
+    print('No program!');
+    return;
+  }
+
+  final view = ProgramView(program.design);
+
+  document.body.children.add(view.root);
+  view.start();
+}
