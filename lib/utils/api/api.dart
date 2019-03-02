1d9fb077122fc7b4e476d125e39b8c4702f66bee
tejainece <tejainece@gmail.com>
2019-03-02 22:22:53 +0100

    Api base

--- lib/utils/api/api.dart
+++ lib/utils/api/api.dart
@@ -1,7 +1,7 @@
 import 'package:common/api/api.dart';
 import 'package:jaguar_resty/jaguar_resty.dart';
 
-final base = Route("http://localhost:10000/api");
+final base = Route("/api");
 
 final programApi = ProgramApi(base);
 
