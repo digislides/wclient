4db793b31a4e25268b6b7b28d858e11222a994ff
tejainece <tejainece@gmail.com>
2019-04-02 14:51:55 +0200

    Media image upload

--- lib/src/apps/media/images/upload/upload.dart
+++ lib/src/apps/media/images/upload/upload.dart
@@ -1,9 +1,10 @@
 import 'dart:html';
 import 'package:path/path.dart' as path;
 
+import 'package:jaguar_resty/jaguar_resty.dart';
 import 'package:angular/angular.dart';
 import 'package:common/models.dart';
-import 'package:common/common.dart';
+import 'package:common/api/api.dart';
 
 import 'package:wclient/src/utils/directives/input_binder.dart';
 
@@ -19,12 +20,42 @@ import 'package:wclient/src/utils/directives/input_binder.dart';
   exports: [],
 )
 class ImageUploadComponent {
+  @ViewChild("file")
+  FileUploadInputElement fileInput;
+
+  final tags = <String>[];
+
   final uploads = <Upload>[];
 
   void onFilePick(List<File> files) {
     for (File f in files) {
-      uploads.add(Upload(path.basenameWithoutExtension(f.name),
-          path.extension(f.name), f.slice()));
+      final up = Upload(
+          path.basenameWithoutExtension(f.name), path.extension(f.name), f);
+      final fr = FileReader();
+      fr.onLoad.listen((_) {
+        up.url = fr.result;
+      });
+      fr.readAsDataUrl(f);
+      uploads.add(up);
+    }
+    fileInput.value = "";
+  }
+
+  void addTag(InputElement el) {
+    final name = el.value;
+    if (name.isEmpty || name.contains(RegExp(r"[^a-zA-Z0-9_\-]"))) return;
+    tags.add(name);
+    el.value = "";
+  }
+
+  void upload() async {
+    for (Upload upload in uploads) {
+      final fr = FileReader();
+      fr.readAsArrayBuffer(upload.file);
+      await fr.onLoad.first;
+      final name = upload.name + upload.extension;
+      await mediaImageApi.create(MediaCreator(name: name, tags: tags),
+          MultipartFile(fr.result, filename: name));
     }
   }
 }
@@ -35,6 +66,8 @@ class Upload {
   String extension;
 
   Blob file;
+
+  String url;
 
   Upload(this.name, this.extension, this.file);
 }
